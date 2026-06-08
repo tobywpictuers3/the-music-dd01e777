08db7c1a801ca9f36f83172bc9c24ad3e5e51364
tobywpictuers3 <tobyw.pictuers3@gmail.com>
2026-06-08 11:26:23 +0300

    TOBY music: add /new preview route for IndexNew

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
+import IndexNew from "./pages/IndexNew";
 import Article from "./pages/Article";
 import Wellness from "./pages/Wellness";
 import Travel from "./pages/Travel";
@@ -54,6 +55,7 @@ const App = () => (
         <ScrollToTopOnRouteChange />
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/new" element={<IndexNew />} />
           <Route path="/article/:id" element={<Article />} />
           <Route path="/wellness" element={<Wellness />} />
           <Route path="/travel" element={<Travel />} />
